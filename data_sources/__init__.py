69d5959407db9a16194c5395d62203de0f94a6f6
frischler <frischler@posteo.de>
2026-02-04 18:46:50 +0100

    Re-arranged datasources

--- data_sources/__init__.py
+++ data_sources/__init__.py
@@ -23,46 +23,51 @@ data_sources: dict[MainMenuOption] = {
         icon=resource_path("assets/data_source_mouse.png"),
         clazz=MouseDataSource,
     ),
+    "eye-tracking-glasses": MainMenuOption(
+        key="eye-tracking-glasses",
+        title="Eye-Tracking Glasses",
+        description="Eye-Tracking using Eye-Tracking glasses\n" +
+                    "with an infrared camera in front of the eye.",
+        icon=resource_path("assets/data_source_pupil.png"),
+        clazz=Pye3dDataSource,
+    ),
+    "webcam-head-tracking": MainMenuOption(
+        key="webcam-head-tracking",
+        title="Webcam Head-Tracking",
+        description="Head-Tracking with just using a Webcam.",
+        icon=resource_path("assets/data_source_pupil.png"),
+        clazz=MediaPipeDataSource,
+    ),
     "orlosky": MainMenuOption(
         key="orlosky",
         title="Orlosky",
-        description="The 3DEyeTracker from Jason Orlosky.",
+        description="The 3DEyeTracker from Jason Orlosky.\n" +
+                    "**Requires an external application to work.**",
         icon=resource_path("assets/data_source_orlosky.png"),
         clazz=OrloskyDataSource,
     ),
     "opentrack": MainMenuOption(
         key="opentrack",
         title="OpenTrack",
-        description="The rotation of your head with OpenTrack.",
+        description="The rotation of your head with OpenTrack.\n" +
+                    "**Requires an external application to work.**",
         icon=resource_path("assets/data_source_opentrack.png"),
         clazz=OpentrackDataSource,
     ),
     "pupil": MainMenuOption(
         key="pupil",
         title="Pupil",
-        description="Pupil Lab's 3d-eye detection.",
+        description="Pupil Lab's 3d-eye detection.\n" +
+                    "**Requires an external application to work.**",
         icon=resource_path("assets/data_source_pupil.png"),
         clazz=PupilDataSource,
     ),
     "eyetrackvr": MainMenuOption(
         key="eyetrackvr",
         title="EyeTrackVR",
-        description="Eye tracking with EyeTrackVR.",
+        description="Eye tracking with EyeTrackVR.\n" +
+                    "**Requires an external application to work.**",
         icon=resource_path("assets/data_source_eyetrackvr.png"),
         clazz=EyeTrackVRDataSource,
-    ),
-    "pye3d": MainMenuOption(
-        key="pye3d",
-        title="Pye3d",
-        description="Integrated Eye-Tracking using pye3D.",
-        icon=resource_path("assets/data_source_pupil.png"),
-        clazz=Pye3dDataSource,
-    ),
-    "mediapipe": MainMenuOption(
-        key="mediapipe",
-        title="MediaPipe",
-        description="Integrated Head-Tracking using MediaPipe.",
-        icon=resource_path("assets/data_source_pupil.png"),
-        clazz=MediaPipeDataSource,
     ),
 }
