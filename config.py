57344284a747a86f3588b4eb2f9dc8fc5d560316
frischler <frischler@posteo.de>
2026-02-06 20:23:04 +0100

    updated info in config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-# meta
 import os
 import sys
 from misc import resource_path
 
+# meta
 APP_SHORT_NAME = "Miranda"
-APP_FULL_NAME = "Miranda Eye Tracking Screen Calibrator"
+APP_FULL_NAME = "Miranda Eye Tracking"
 APP_VERSION = "1.0.0-alpha.1"
 APP_ICON_LINUX = resource_path("assets/icon.png")
 APP_ICON_WINDOWS = resource_path("assets/icon.ico")
-APP_DESCRIPTION = "The tool for calibrating eye and head tracker input to match your gaze on the screen."
-APP_LINK_WEBSITE = "https://gaming.ifb-stiftung.de/eyes-on-disabilities-home/"
-APP_LINK_CODE = "https://codeberg.org/eyes-on-disabilities/miranda-eye-tracking-screen-calibrator"
+APP_DESCRIPTION = "An eye-tracking-toolkit for calibrating eye- and head-tracker inputs to match your screen gaze."
+APP_LINK_WEBSITE = "https://eyes-on-disabilities.org/"
+APP_LINK_CODE = "https://codeberg.org/eyes-on-disabilities/miranda-eye-tracking"
 
 LINUX_CONFIG_DIR = os.path.join(
     os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config")),
